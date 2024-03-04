36f2ca440be615d748f04176aa691b1eb2ed3565
JonasBeans <boon.jonas.git@outlook.com>
2024-03-04 22:20:03 +0100

    Init commit

---
+++ read-me.md
@@ -0,0 +1,5 @@
+# Portfolio project
+## Color pallete
+[color palette](https://colorhunt.co/palette/eadfb49bb0c151829bf6995c)
+## Font
+[fonts](https://www.dafont.com/lt-renovate.font)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogPostsComponent } from './blog-posts/blog-posts.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostsComponent,
+    BlogPostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
